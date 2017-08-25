367151c3dd068a83d2f2cf871a6c7c349e9e3921
Edwin Cruz <Edwin Cruz>
2017-08-25 16:52:23 -0600

    Add project files.

---
+++ Proyecto1_TBBDD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto1_TBBDD.Startup))]
+namespace Proyecto1_TBBDD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
